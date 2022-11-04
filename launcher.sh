087e773772ae0d0d99cd2b69416e9d665ee68ecb
Orest Hudyma <orestgudyma@gmail.com>
2022-11-04 12:50:15 +0200

    In progress

--- launcher.sh
+++ launcher.sh
@@ -9,8 +9,10 @@ if hash py; then
     echo "Python detected under py alias"
 elif hash python3; then
   alias py='python3'
+  echo "Python3 detected"
 elif hash python; then
   alias py='python'
+  echo "Python detected"
 fi
 
 py -u main.py
