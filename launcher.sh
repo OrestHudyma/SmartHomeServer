8199e3c305092aa148eb24d7d0c67abe5c589c69
Orest Hudyma <orestgudyma@gmail.com>
2022-11-04 12:20:52 +0200

    In progress

--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
 git pull
+#python3 main.py
+
+if hash py; then
+    echo "Python detected under py alias"
+elif hash python3; then
+  alias py='python3'
+elif hash python; then
+  alias py='python'
+fi
+
+py -u main.py
